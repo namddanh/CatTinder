7c4f72d92cfaf6c1c6f27d721160d3cfe98b00e4
namddanh <namddanh@gmail.com>
2025-05-01 21:58:21 -0400

    added first template for preview profile

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,98 @@
 <script setup></script>
 
 <template>
-  <div>
-    <h1>Hello cat app</h1>
+  <header class="cat-header">
+    <font-awesome-icon icon="fa-solid fa-user" class="icon" />
+
+    <font-awesome-icon icon="fa-solid fa-sliders" class="icon" />
+    <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="icon" />
+    <font-awesome-icon icon="fa-solid fa-paw" class="icon" />
+    <font-awesome-icon icon="fa-solid fa-circle-right" class="icon" />
+    <font-awesome-icon icon="fa-solid fa-circle-left" class="icon" />
+    <!-- 😻 -->
+    <h3>
+      <font-awesome-icon
+        icon="fa-solid fa-cat"
+        class="icon"
+      />CatTinder<font-awesome-icon icon="fa-solid fa-cat" class="icon" />
+    </h3>
+  </header>
+
+  <div class="cat-preview-container">
+    <div class="card-preview-card">
+      <img
+        class="cat-preview-img"
+        src="https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg"
+        alt=""
+      />
+      <div class="cat-preview-description">
+        <div class="cat-preview-intro">
+          <h3>
+            Kip 3
+            <font-awesome-icon icon="fa-solid fa-check-circle" class="icon" />
+          </h3>
+        </div>
+        <div class="cat-preview-location">
+          <h4>
+            <font-awesome-icon icon="fa-solid fa-house" class="icon" />Lives in
+            Toronto
+          </h4>
+          <h4>
+            <font-awesome-icon icon="fa-solid fa-location-dot" class="icon" />
+            27 km away
+          </h4>
+        </div>
+        <div class="cat-preview-interests">
+          <h3>
+            <font-awesome-icon icon="fa-solid fa-paw" class="icon" />Interests
+          </h3>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .cat-header {
+    text-align: center;
+    background-color: #a74c49;
+    color: rgb(247 188 120);
+  }
+
+  .cat-preview-container {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: bisque;
+  }
+
+  .card-preview-card {
+    display: flex;
+    flex-direction: column;
+    width: 750px;
+    overflow: hidden;
+    border-radius: 3%;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+  }
+
+  .cat-preview-container .cat-preview-img {
+    height: 500px;
+    width: 100%;
+  }
+
+  .cat-preview-description {
+    align-self: left;
+    text-align: left;
+    padding: 10px;
+    background-color: rgb(247 188 120);
+  }
+
+  .cat-preview-description h3,
+  .cat-preview-description h4 {
+    color: #a74c49;
+    font-weight: bold;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,41 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUser,
+  faClock,
+  faCat,
+  faSquareCheck,
+  faCheckCircle,
+  faCheckToSlot,
+  faSliders,
+  faLocationDot,
+  faMagnifyingGlass,
+  faHouse,
+  faPaw,
+  faCircleRight,
+  faCircleLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import "./assets/main.css";
+library.add(
+  faUser,
+  faClock,
+  faCat,
+  faSquareCheck,
+  faCheckCircle,
+  faCheckToSlot,
+  faSliders,
+  faLocationDot,
+  faMagnifyingGlass,
+  faHouse,
+  faPaw,
+  faCircleRight,
+  faCircleLeft
+);
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
