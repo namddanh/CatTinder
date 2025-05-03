b8f4661873ca41beaa6c9dc3c4387e14fb90e774
Nam <namddanh@gmail.com>
2025-05-03 11:25:20 -0400

    getting started with routers

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import router from "./router/index";
+
 import {
   faUser,
   faClock,
@@ -43,5 +45,6 @@ library.add(
 
 const app = createApp(App);
 
+app.use(router);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
---
+++ src/views/LikedView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>LikedView</h1>
+  </div>
+</template>
